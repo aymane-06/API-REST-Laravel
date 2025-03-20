f4409ad9165d22f63e78c726944efb75d378498e
aymane-06 <aymane.himame@gmail.Com>
2025-03-20 23:12:28 +0000

    Update API routes to use JWT middleware for user and application endpoints

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,22 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\JWTMiddleware;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/user', [UserController::class,'index'])->middleware('auth:sanctum');
-Route::apiResource('user', UserController::class)->middleware('auth:sanctum');
+Route::apiResource('user', UserController::class)->middleware(JWTMiddleware::class);
 Route::apiResource('offers', 'App\Http\Controllers\OfferController');
 
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::post('/logout', [AuthController::class, 'logout'])->middleware(JWTMiddleware::class);
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+// Application routes
+Route::apiResource('applications', 'App\Http\Controllers\ApplicationController')->middleware(JWTMiddleware::class);
+Route::patch('applications/{application}/status', 'App\Http\Controllers\ApplicationController@changeStatus')->middleware(JWTMiddleware::class);
